49f2272908e8df226baf6a6104a13a404a7c780e
theItalianDev <theItalianDev@MinidiGiuseppe.wind3.hub>
2022-01-26 15:15:49 +0100

    final version

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
           <title>Login | Dashboard3</title>
         </Head>
         <Flex direction="column" justifyContent="center" alignItems="center" width="100vw" height="100vh" bgGradient="linear(to-br, teal.400, purple.300)">
-          <Text fontSize="6xl" fontWeight="bold" color="white">Dashboard3</Text>
+          <Text fontSize="5xl" fontWeight="bold" color="white">Dashboard3</Text>
           <Button colorScheme="purple" size="lg" mt="6" onClick={() => authenticate({
             signingMessage: "Sign required to login in Dashboard3"
           })} disabled={isAuthenticating}>Login with Metamask</Button>
